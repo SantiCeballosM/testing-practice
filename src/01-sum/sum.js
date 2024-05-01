f47acd44917da36c68d2e8a840c723182493c83e
SantiCeballosM <107371068+SantiCeballosM@users.noreply.github.com>
2024-04-30 22:55:33 -0500

    testing al dia

--- src/01-sum/sum.js
+++ src/01-sum/sum.js
@@ -1,6 +1,8 @@
-
-
-function sum(a, b) {
-    return a + b;
+function sum(num1, num2) {
+  if (num1 === undefined || num2 === undefined) {
+    return "hubo error con los argumentos";
   }
-  module.exports = sum;
+  return num1 + num2;
+}
+
+module.exports = sum;
--- src/01-sum/sum.test.js
+++ src/01-sum/sum.test.js
@@ -1,16 +1,17 @@
 
-const sum = require('./sum');
+const sum = require("./sum");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test("adds 1 + 2 to equal 3", () => {
+  const result = sum(1, 2);
+  expect(result).toBe(3);
 });
 
-test ("add 10 + 10 to equal 20", () => {
-    const result = sum(10, 10);
-    expect(result).toBe(20);
+test("add 10 + 10 to equal 20", () => {
+  const result = sum(10, 10);
+  expect(result).toBe(20);
 });
 
-test ("should return error ir arguments are not passed", () => {
-    const result = sum();
-    expect(result).toBe("Hubo con los argumentos");
+test("deberia retornar un error", () => {
+  const result = sum();
+  expect(result).toBe("hubo error con los argumentos");
 });
